677d764d943362e96f39094529d8156fbafb55a2
tobseyltd <mail@tobsey.limited>
2023-07-20 07:52:30 +0200

    Task List, Task Item Layout

---
+++ src/routes/task-manager/+layout.svelte
@@ -0,0 +1,3 @@
+<div class="box-content flex-wrap h-full overflow-hidden flex-it md:p-20">
+	<slot />
+</div>
---
+++ src/routes/task-manager/+page.svelte
@@ -0,0 +1,69 @@
+<div class="h-full p-10">
+	<div class="mb-6 text-2xl text-white">Some List</div>
+	<button class="flex items-start mb-3 text-xl font-bold text-white cursor-pointer hover:underline">
+		+ Add List
+	</button>
+	<div class="h-full flex-it">
+		<div class="flex-row h-full flex-it rounded-xl">
+			<!-- TASK LIST START -->
+			<div class="h-full max-w-sm min-h-full m-2 my-0 flex-it w-80">
+				<div class="max-h-full border-2 border-gray-500 bg-slate-400 flex-it rounded-xl">
+					<div class="m-3 flex-it">
+						<div class="flex-row flex-it">
+							<div class="mr-2 text-xl font-bold text-left">List 1</div>
+							<div class="flex items-center hover:text-red-600">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-trash"
+									viewBox="0 0 16 16"
+								>
+									<path
+										d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+									/>
+									<path
+										fill-rule="evenodd"
+										d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+									/>
+								</svg>
+							</div>
+						</div>
+					</div>
+					<div class="p-2 overflow-x-hidden overflow-y-auto with-scrollbar">
+						<!-- TASK ITEM START -->
+						<div class="p-2 mb-2 border border-solid cursor-pointer flex-it rounded-xl bg-slate-500">
+							<div class="flex-it">
+								<div class="flex-row flex-it">
+									<div class="flex flex-1">Some Task</div>
+									<div class="flex items-end hover:text-red-600">
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="16"
+											height="16"
+											fill="currentColor"
+											class="bi bi-trash"
+											viewBox="0 0 16 16"
+										>
+											<path
+												d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+											/>
+											<path
+												fill-rule="evenodd"
+												d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+											/>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- TASK ITEM END -->
+					</div>
+					<button class="flex p-2 underline"> + Add Task </button>
+				</div>
+			</div>
+			<!-- TASK LIST END -->
+		</div>
+	</div>
+</div>
