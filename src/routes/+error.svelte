a71041e372f6a9539e28d5feb423c2d2938a17fe
tobseyltd <mail@tobsey.limited>
2023-07-25 09:09:42 +0200

    added error page and components alias in svelte cfg

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="items-center justify-center h-full text-white flex-it">
+	<div class="text-4xl font-bold">
+		{$page.status} - {$page.error?.message}
+	</div>
+	<a href="/twitter-clone">Go back to Home Page</a>
+</div>
