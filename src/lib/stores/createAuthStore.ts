d09b4766ef5b7002b85eef7a8d3f98acc7baf810
tobseyltd <mail@tobsey.limited>
2023-07-28 11:11:27 +0200

    snackbar on auth

--- src/lib/stores/createAuthStore.ts
+++ src/lib/stores/createAuthStore.ts
@@ -1,7 +1,10 @@
 import { authenticate } from '@api/auth';
+import { getUIContext } from '@components/context/UI';
 import { writable } from 'svelte/store';
+import { v4 as uuid4 } from 'uuid';
 
 export function createAuthStore(authType: string) {
+	const { addSnackbar } = getUIContext();
 	const loading = writable(false);
 
 	async function authUser(form: { email: string; password: string }) {
@@ -9,8 +12,17 @@ export function createAuthStore(authType: string) {
 
 		try {
 			await authenticate(form, authType);
+			addSnackbar({
+				message: 'Welcome back!',
+				type: 'success',
+				id: uuid4()
+			});
 		} catch (ERROR: any) {
-			console.log(ERROR.message);
+			addSnackbar({
+				message: ERROR.message,
+				type: 'error',
+				id: uuid4()
+			});
 			loading.set(false);
 		}
 	}
